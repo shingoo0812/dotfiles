4a59136cae128b5979ccb69f41e42522fca75d4a
shingoo0812 <shingoo0812@gmail.com>
2024-03-15 20:18:14 +0900

    add tmux config

---
+++ install/bootstrap.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#
+# bootstrap installs things.
+#
+
+
+cd "$(dirname "$0")/.."
+
+DOTFILES=$(pwd -P)
+
+set -e
+
+echo ''
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+install_dotfiles () {
+  info 'installing dotfiles'
+  
+  local overwrite_all=false backup_all=false skip_all=false
+
+  find -H "$DOTFILES" -maxdepth 2 -name 'links.prop' -not -path '*.git*' | while read linkfile
+  do
+    cat "$linkfile" | while read line
+    do
+        local src dst dir
+        src=$(eval echo "$line" | cut -d '=' -f 1)
+        dst=$(eval echo "$line" | cut -d '=' -f 2)
+        dir=$(dirname $dst)
+
+        mkdir -p "$dir"
+        link_file "$src" "$dst"
+    done
+  done
+}
