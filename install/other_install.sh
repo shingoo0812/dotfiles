b434c8792eabdcfa7f68b7e7d020baecd72eec2b
shingoo0812 <shingoo0812@gmail.com>
2024-06-12 06:43:28 +0900

    modify gitignore

---
+++ install/other_install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+apt update && \
+apt-get update && \
+apt install -y curl git jq git ripgrep tar unzip wget python3 python3-pip python3-venv tmux man gdb openssh-client nodejs zsh xsel
